478dfbdbb76206d0c896055eec1530002717c82d
wesam <wesammoawia2004@gmail.com>
2023-10-26 18:14:31 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,7 +22,7 @@ int check_prime(int n, int i)
 {
 if (i >= n && n > 1)
 return (1);
-else if (n % i == 0 || n<= 1)
+else if (n % i == 0 || n <= 1)
 return (0);
 else
 return (check_prime(n, i + 1));
