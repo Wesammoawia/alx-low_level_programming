a9a35873e765ef6b4c97babb2ae0031e75ac8784
wesam <wesammoawia2004@gmail.com>
2023-11-06 17:16:01 -0800

    dogz

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return;
 if (d->name == NULL)
-d->name = "(null)";
+d->name = "(nil)";
 if (d->owner == NULL)
-d->owner = "(null)";
+d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
